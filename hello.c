05846e5e8dc18695756dee49250031cd040e37c2
anuescapist <annuu088@gmail.com>
2023-10-05 08:40:53 -0400

    fristcommit

---
+++ hello.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+    This is a simple "Hello, World!" program in C.
+    It demonstrates the basic structure of a C program.
+    The program prints "Hello, World!" to the console.
+*/
+
+int main() {
+    // Print "Hello, World!" to the console
+    printf("Hello, World!");
+
+    // Return 0 to indicate successful program execution
+    return 0;
+}
+
